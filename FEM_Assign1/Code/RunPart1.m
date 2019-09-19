206a6727ab8fc5b059b30017afb3a62d736bfcb9
Peter Kardos <kardospeter1994@hotmail.com>
2019-09-20 00:35:35 +0200

    refactored MakeAnimation into its own file

--- FEM_Assign1/Code/RunPart1.m
+++ FEM_Assign1/Code/RunPart1.m
@@ -97,25 +97,3 @@ function [U, M, p, t] = RunSimulationGFEM(meshSize, InitialData)
 
     U = SolverCN(M, C, xi, timeStep, numIters);
 end
-
-
-function MakeAnimation(p, t, U, fileName, frameTime)
-    h = figure;
-    axis tight manual % this ensures that getframe() returns a consistent size
-    for frameIdx = 1:size(U, 2)
-        % Draw plot
-        pdesurf(p, t, U(:,frameIdx))
-        drawnow
-        % Capture the plot as an image
-        frame = getframe(h);
-        im = frame2im(frame); 
-        [imind,cm] = rgb2ind(im,256); 
-        % Write to the GIF File
-        if frameIdx == 1 
-            imwrite(imind,cm,[fileName, '.gif'],'gif', 'Loopcount',inf,'DelayTime',frameTime); 
-        else 
-            imwrite(imind,cm,[fileName, '.gif'],'gif','WriteMode','append','DelayTime',frameTime); 
-        end
-    end
-end
-
